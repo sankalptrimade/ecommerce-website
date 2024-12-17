93a87e73abd9028a0b2ad63a29be2f635e4396a6
sankalptrimade <sankalptrimade0014@gmail.com>
2024-12-18 01:26:26 +0530

    Basic Code of Project

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Navbar from './components/Navbar'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Navbar />
+  </StrictMode>,
+)
